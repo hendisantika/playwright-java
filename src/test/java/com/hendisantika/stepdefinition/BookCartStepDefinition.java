15892d361dde2f4f08515ab30f2a1ea97c8043f6
hendisantika <hendisantika@yahoo.co.id>
2023-10-27 16:04:23 +0700

    #18 - Add userAlreadyLoggedIn in BookCartStepDefinition ✏️📗:octocat:⬆

--- src/test/java/com/hendisantika/stepdefinition/BookCartStepDefinition.java
+++ src/test/java/com/hendisantika/stepdefinition/BookCartStepDefinition.java
@@ -2,6 +2,8 @@ package com.hendisantika.stepdefinition;
 
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
+import io.cucumber.java.en.Given;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +18,17 @@ import com.microsoft.playwright.Playwright;
 public class BookCartStepDefinition {
     private Page page;
     private Playwright playwright;
+
+    @Given("user already logged in")
+    public void userAlreadyLoggedIn() {
+        //navigate to application
+        page.navigate("https://bookcart.azurewebsites.net/");
+
+        //run the automation
+        page.getByText("Login").click();
+        page.getByLabel("Username").fill("ortoni");
+        page.getByLabel("Password").fill("Pass1234$");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions()
+                .setName("Login")).last().click();
+    }
 }
