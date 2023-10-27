e029d32eb99e89eb5b7476884b4f874c42c10a8f
hendisantika <hendisantika@yahoo.co.id>
2023-10-27 16:02:12 +0700

    #14 - Add QuickSearchRunner ✏️📗:octocat:⬆

---
+++ src/test/java/com/hendisantika/cucumber/QuickSearchRunner.java
@@ -0,0 +1,23 @@
+package com.hendisantika.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : playwright-java
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/27/23
+ * Time: 16:00
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(glue = {"playwright.hendisantika.stepdefinition"}
+        , features = {"src/test/resources/features/QuickSearch.feature"}
+        , plugin = "json:target/cucumber-result/json/QuickSearch.json"
+)
+public class QuickSearchRunner {
+}
