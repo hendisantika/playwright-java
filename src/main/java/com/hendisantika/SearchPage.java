225c6dc668f4af48ea0b575c30fee3c9a2b744e2
hendisantika <hendisantika@yahoo.co.id>
2023-10-27 15:45:33 +0700

    #4 - Add constructor SearchPage ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/SearchPage.java
+++ src/main/java/com/hendisantika/SearchPage.java
@@ -13,10 +13,14 @@ import com.microsoft.playwright.Page;
  * To change this template use File | Settings | File Templates.
  */
 public class SearchPage {
-    private Page page;
+    private final Page page;
 
     private final String locator_searchBar = "#searchBar";
     private final String locator_hiddenBooks = "li.ui-screen-hidden";
     private final String locator_visibleBooks = "li:not(.ui-screen-hidden)";
     private final String locator_visibleBookTitles = "li:not(.ui-screen-hidden) h2";
+
+    public SearchPage(Page page) {
+        this.page = page;
+    }
 }
