b581f39af9fbcb47c717095464d08ad2644c701a
hendisantika <hendisantika@yahoo.co.id>
2023-10-27 16:04:08 +0700

    #17 - Add BookCartStepDefinition ✏️📗:octocat:⬆

---
+++ src/test/java/com/hendisantika/stepdefinition/BookCartStepDefinition.java
@@ -0,0 +1,19 @@
+package com.hendisantika.stepdefinition;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : playwright-java
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/27/23
+ * Time: 16:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookCartStepDefinition {
+    private Page page;
+    private Playwright playwright;
+}
