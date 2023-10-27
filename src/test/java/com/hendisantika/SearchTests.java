bb2e02c4de43322d6ddbb57bc2698b0ac7b747ef
hendisantika <hendisantika@yahoo.co.id>
2023-10-27 15:50:07 +0700

    #11 - Add searchForPartialTitle in SearchTests ✏️📗:octocat:⬆

--- src/test/java/com/hendisantika/SearchTests.java
+++ src/test/java/com/hendisantika/SearchTests.java
@@ -2,6 +2,8 @@ package com.hendisantika;
 
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -23,5 +25,21 @@ public class SearchTests extends BaseTests {
         searchPage.search(title);
         assertEquals(searchPage.getNumberOfVisibleBooks(), 1, "Number of visible books");
         assertTrue(searchPage.getVisibleBooks().contains(title), "Title of visible book");
+    }
+
+    @Test
+    public void searchForPartialTitle() {
+        searchPage.search("Test");
+
+        List<String> expectedBooks = List.of(
+                "Test Automation in the Real World",
+                "Experiences of Test Automation",
+                "Agile Testing",
+                "How Google Tests Software",
+                "Java For Testers"
+        );
+
+        assertEquals(searchPage.getNumberOfVisibleBooks(), expectedBooks.size(), "Number of visible books");
+        assertEquals(searchPage.getVisibleBooks(), expectedBooks, "Titles of visible books");
     }
 }
